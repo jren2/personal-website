64243a9dc1fe9b8ac1746eadb0179f64d512e02f
Jason Ren <jasoncren22@gmail.com>
2022-08-06 17:26:12 -0400

    added firebase analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 import { App } from './App'
+import {
+  api_key, app_id, measurement_id, messaging_sender_id, storage_bucket,
+} from './config'
 
 import './index.css'
+
+const firebaseConfig = {
+  apiKey: api_key,
+  authDomain: 'jason-ren.firebaseapp.com',
+  projectId: 'jason-ren',
+  storageBucket: storage_bucket,
+  messagingSenderId: messaging_sender_id,
+  appId: app_id,
+  measurementId: measurement_id,
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebaseApp)
 
 const app = document.getElementById('app')
 
