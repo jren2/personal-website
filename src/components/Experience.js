10900c55b72c1afda2d8b77fd616a892fdc99786
Jason Ren <jasoncren22@gmail.com>
2022-09-02 12:55:07 -0400

    updated experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,12 +10,24 @@ const Experience = () => (
         <div className="experienceSubtitle mb-10 text-center w-fit mx-auto text-3xl font-medium tracking-wider text-slate-200">
           Experience
         </div>
-
+        <div className="flex xs:flex-col xs:items-center md:flex-row mt-10 mb-6 gap-4">
+          <img alt="ren logo" className="mt-6 h-1/2 rounded-3xl" width="180" src={require('./images/ren.png')} />
+          <div className="text-xl xs:text-center md:text-left font-medium tracking-normal text-slate-200">
+            Software Developer Intern
+            <a href="https://www.renenergyglobal.com/" target="blank_" className="text-teal-400 inline"> @ Ren Energy </a>
+            <div className="mt-1 mb-4 text-slate-400 text-sm">
+              August 2022 - Present
+            </div>
+            <div className="list-element my-2 text-slate-400 text-base font-normal tracking-wide">
+              &emsp; Build production level features with Angular based tech stack
+            </div>
+          </div>
+        </div>
         <div className="flex xs:flex-col xs:items-center md:flex-row mt-16 mb-6 gap-4">
-          <img alt="juni logo" className="mt-6 w-1/2 h-1/2 rounded-3xl" width="1000" src={require('./images/juni.png')} />
+          <img alt="juni logo" className="mt-6 h-1/2 rounded-3xl" width="180" src={require('./images/juni.png')} />
           <div className="text-xl xs:text-center md:text-left font-medium tracking-normal text-slate-200">
             Computer Science Instructor
-            <div className="text-teal-400 inline"> @ Juni Learning </div>
+            <a href="https://junilearning.com/" target="blank_" className="text-teal-400 inline"> @ Juni Learning </a>
             <div className="mt-1 mb-4 text-slate-400 text-sm">
               June 2021 - Present
             </div>
